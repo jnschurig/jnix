d3803853fab905c33243bf30453a77cdc75dfc41
James Schurig <jnschurig@gmail.com>
2024-06-25 19:47:18 -0600

    one more tweak

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      backupFileExtension = "bak";
+      backupFileExtension = "hm-backup";
     };
 
     # nixvim = {
