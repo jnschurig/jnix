c35ba89a973a275fa603169ec50fb7a8f743dbdb
James Schurig <jnschurig@gmail.com>
2024-06-27 19:50:25 -0600

    Update fzf.nix

--- home-manager/modules/fzf.nix
+++ home-manager/modules/fzf.nix
@@ -2,5 +2,9 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+      changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git";
+      changeDirWidgetOptions = [
+        ''--preview "bat --color=always --style=numbers --line-range=:500 {}"''
+      ];
   };
 }
