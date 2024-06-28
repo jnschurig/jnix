d78f522cf478a77a10af51c3b9df3e38f002c7f8
James Schurig <jnschurig@gmail.com>
2024-06-27 19:10:12 -0600

    hope this one works

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -9,28 +9,6 @@
     # enableAutosuggestions = true;
     autosuggestion.enable = true;
    
-    # Trying out removing this.
-    # syntaxHighlighting.enable = true;
-
-    # shellAliases =
-    #   let
-    #     flakeDir = "~/nix";
-    #   in {
-    #   rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
-    #   upd = "nix flake update ${flakeDir}";
-    #   upg = "sudo nixos-rebuild switch --upgrade --flake ${flakeDir}";
-
-    #   hms = "home-manager switch --flake ${flakeDir}";
-
-    #   conf = "nvim ${flakeDir}/nixos/configuration.nix";
-    #   pkgs = "nvim ${flakeDir}/nixos/packages.nix";
-
-    #   ll = "ls -l";
-    #   v = "nvim";
-    #   se = "sudoedit";
-    #   ff = "fastfetch";
-    # };
-
     history = {
       size = 10000;
       save = 10000;
@@ -40,10 +18,10 @@
       # path = "${config.xdg.dataHome}/zsh/history";
     };
 
-    initExtraFirst = ''
-      # source ${pkgs.zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-    '';
+    # initExtraFirst = ''
+    #   # source ${pkgs.zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+    #   source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+    # '';
 
     # Aliases
     shellAliases = import ./shell/aliases.nix;
@@ -81,16 +59,15 @@
         src = ./p10k-config;
         file = "p10k.zsh";
       }
-      {
-        name = "zsh-extra";
-        src = ./zsh;
-        file = "extra.zsh";
-      }
+      # {
+      #   name = "zsh-extra";
+      #   src = ./zsh;
+      #   file = "extra.zsh";
+      # }
     ];
 
     localVariables = {
       POWERLEVEL9K_CONFIG_FILE = [
-        # ./p10k-config/zsh-theme-gruvbox-material-dark.zsh
         ./p10k-config/p10k.zsh
       ];
       POWERLEVEL9K_INSTANT_PROMPT = "quiet";
@@ -99,12 +76,6 @@
       # QMK_HOME = "${lib.makeBinPath pkgs.qmk_firmware}/..";
       ZSH_AUTOSUGGEST_USE_ASYNC = true;
       # ZSH_AUTOSUGGEST_STRATEGY = "(history match_prev_cmd)";
-
-      # work only
-      # CLOUDSDK_PYTHON = "${pkgs.python311}/bin/python";
-      # VAULT_ADDR = "https://vault.infra.rechargeapps.net:443";
-      # OBJC_DISABLE_INITIALIZE_FORK_SAFETY = "YES";
-      # INCLUDE_WORK_FUNCS = true;
     };
 
   };
