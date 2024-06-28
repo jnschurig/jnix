c11dcacff6d1133968470ba433d2e976b7f12037
James Schurig <jnschurig@gmail.com>
2024-06-27 22:06:57 -0600

    here we go with the sound

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ./packages.nix
       ./modules/user.nix
+      # inputs.nix-gaming.nixosModules.<module name>
     ];
 
   # Bootloader.
@@ -62,33 +63,33 @@
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+
+    lowLatency = {
+      # enable this module
+      enable = true;
+      # defaults (no need to be set unless modified)
+      quantum = 64;
+      rate = 48000;
+    };
   };
+
+  # make pipewire realtime-capable
+  security.rtkit.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.james = {
-  #   isNormalUser = true;
-  #   description = "James Schurig";
-  #   extraGroups = [ "networkmanager" "wheel" ];
-  #   packages = with pkgs; [
-  #     kdePackages.kate
-  #   #  thunderbird
-  #   ];
-  # };
 
   # Enable Flakes
   nix.settings = {
