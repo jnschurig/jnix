22cfb5fe049dad4586e92cc0d73253833be2c0f6
James Schurig <jnschurig@gmail.com>
2024-06-30 11:26:50 -0600

    pulling back a bit

--- home-manager/modules/bundle.nix
+++ home-manager/modules/bundle.nix
@@ -3,17 +3,13 @@
 , ...
 }: {
   imports = [
-    ./devenv.nix
     ./fzf.nix
     ./git.nix
-    # moving to apps.nix
-    # ./kitty.nix
     ./pyenv.nix
     ./zoxide.nix
     ./zsh.nix
     # ./cursor.nix
     # ./htop.nix
-    # ./alacritty.nix
 
     # ./wms/hyprland.nix
     # ./wms/waybar.nix
