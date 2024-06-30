cd2f23782e02c574a1aab9e35e9f41a77fd941be
James Schurig <jnschurig@gmail.com>
2024-06-29 22:57:00 -0600

    got it. pyenv config should be working now...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,7 +16,7 @@
       TERMINAL = "kitty";
     };
 
-    file.".profile" = ''
+    file.".profile".text = ''
       export PYENV_ROOT="$HOME/.pyenv"
       export PATH="$PYENV_ROOT/bin:$PATH"
       eval "$(pyenv init --path)"
