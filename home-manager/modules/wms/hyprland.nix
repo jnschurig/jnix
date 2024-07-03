18ba956b601a91cd32b9af4e883646d927defd40
James Schurig <jnschurig@gmail.com>
2024-07-03 15:40:20 -0600

    Added monitor and removed alacritty in favor of kitty.

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -7,7 +7,7 @@
       "$mainMod" = "SUPER";
 
       # monitor = ",1920x1080@90,auto,1";
-      # monitor = ",2560x1600@165,auto,1";
+      monitor = "WL-1,2560x1600@165,auto,1";
 
       env = [
         "XDG_CURRENT_DESKTOP,Hyprland"
@@ -124,7 +124,7 @@
       # bind = [
       #   "$mainMod, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
 
-      #   "$mainMod, Return, exec, alacritty"
+      #   "$mainMod, Return, exec, kitty"
       #   "$mainMod, Q, killactive,"
       #   "$mainMod, M, exit,"
       #   "$mainMod, E, exec, dolphin"
@@ -194,10 +194,10 @@
       #   ", XF86MonBrightnessUp, exec, brightnessctl set +5% "
 
       #   # Configuration files
-      #   ''$mainMod SHIFT, N, exec, alacritty -e sh -c "rb"''
-      #   ''$mainMod SHIFT, C, exec, alacritty -e sh -c "conf"''
-      #   ''$mainMod SHIFT, H, exec, alacritty -e sh -c "nvim ~/nix/home-manager/modules/wms/hyprland.nix"''
-      #   ''$mainMod SHIFT, W, exec, alacritty -e sh -c "nvim ~/nix/home-manager/modules/wms/waybar.nix''
+      #   ''$mainMod SHIFT, N, exec, kitty -e sh -c "rb"''
+      #   ''$mainMod SHIFT, C, exec, kitty -e sh -c "conf"''
+      #   ''$mainMod SHIFT, H, exec, kitty -e sh -c "nvim ~/nix/home-manager/modules/wms/hyprland.nix"''
+      #   ''$mainMod SHIFT, W, exec, kitty -e sh -c "nvim ~/nix/home-manager/modules/wms/waybar.nix''
       #   '', Print, exec, grim -g "$(slurp)" - | swappy -f -''
 
       #   # Waybar
