243534e7bcb1170d2a2c61edec22cf255ce7f4da
James Schurig <jnschurig@gmail.com>
2024-11-22 19:15:04 -0700

    rofi

---
+++ home-manager/modules/rofi.nix
@@ -0,0 +1,13 @@
+{
+  home.file = {
+    ".config/rofi/poweroff.sh" = {
+      source = ./wms/scripts/poweroff.sh;
+      executable = true;
+    };
+
+    ".config/rofi/rofi-power-menu.sh" = {
+      source = ./wms/scripts/rofi-power-menu.sh;
+      executable = true;
+    };
+  };
+}
