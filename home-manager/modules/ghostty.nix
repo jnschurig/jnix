710fb1dc74b9a8d776cdadbdf022055cfd1e5b07
James Schurig <james.schurig@rechargeapps.com>
2025-01-13 10:15:30 -0700

    custom rose-pine-moon theme

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -2,4 +2,8 @@
   home.file.".config/ghostty/config" = {
     source = ./ghostty/config;
   };
+
+  home.file.".config/ghostty/themes/rose-pine-moon-custom" = {
+    source = ./ghostty/themes/rose-pine-moon-custom;
+  };
 }
