27a2cc58491cf9a55f802e9cfbf6b645f104a1fb
James Schurig <jnschurig@gmail.com>
2025-01-13 21:14:30 -0700

    cleanup of commented code

--- home-manager/modules/rofi.nix
+++ home-manager/modules/rofi.nix
@@ -18,22 +18,4 @@
     };
   };
 
-  # services.sxhkd = {
-  #   enable = true;
-  #   keybindings = {
-  #     "super + space" = "pkill rofi || rofi -show drun -show-icons";
-  #     "super + shift + P" = "~/.config/waybar/scripts/poweroff.sh";
-  #   };
-  # };
-
-  # A gnome setting. It didn't really work anyway...
-  # dconf.settings = {
-  #   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-  #     binding = "<Super>space";
-  #     command = "pkill rofi || rofi -show drun -show-icons";
-  #     name = "Rofi";
-  #   };
-  # };
-
-
 }
