d0c207a3cbab82694c25fc27cffbdae3f3954e6a
James Schurig <jnschurig@gmail.com>
2025-02-01 11:13:33 -0700

    save

--- pacman.sh
+++ pacman.sh
@@ -4,3 +4,4 @@ paru
 sudo pacman -S swayidle ghostty nautilus popsicle vscode fwupd neovim lazygit
 sudo pacman -S discord lutris steam strawberry
 paru -S dolphin-emu-git dolphin-emu-primehack-git
+sudo pacman -Syu --ignore miniupnpc
