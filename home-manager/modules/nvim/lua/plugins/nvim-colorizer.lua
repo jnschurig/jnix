b09e951405e496c3c8cbdc58a632c9c884c9b00c
James Schurig <jnschurig@gmail.com>
2025-03-07 10:37:06 -0700

    colorizer-nvim adjustment

--- home-manager/modules/nvim/lua/plugins/nvim-colorizer.lua
+++ home-manager/modules/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,6 +1,21 @@
 return {
-  "catgoose/nvim-colorizer.lua",
-  event = "BufReadPre",
-  opts = { -- set to setup table
+  {
+    "catgoose/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        filetypes = { "*" }, -- Enable for all filetypes
+        user_default_options = {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          names = true, -- "Name" colors like Blue or Cyan
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          hsl_fn = true, -- CSS hsl() and hsla() functions
+          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          mode = "background", -- Set display mode
+        },
+      })
+    end,
   },
 }
