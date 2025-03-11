9666eeba9d98b66c29e330fd10eb60eb50a124bb
James Schurig <jnschurig@gmail.com>
2025-03-10 20:34:48 -0600

    turning off color names

--- home-manager/modules/nvim/lua/plugins/nvim-colorizer.lua
+++ home-manager/modules/nvim/lua/plugins/nvim-colorizer.lua
@@ -7,7 +7,7 @@ return {
         user_default_options = {
           RGB = true, -- #RGB hex codes
           RRGGBB = true, -- #RRGGBB hex codes
-          names = true, -- "Name" colors like Blue or Cyan
+          names = false, -- "Name" colors like Blue or Cyan
           RRGGBBAA = true, -- #RRGGBBAA hex codes
           rgb_fn = true, -- CSS rgb() and rgba() functions
           hsl_fn = true, -- CSS hsl() and hsla() functions
