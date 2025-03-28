8ce7cde56ed080793a0c5713ef39034af3c63f4b
James Schurig <jnschurig@gmail.com>
2025-03-27 23:14:22 -0600

    just let me disable please

--- home-manager/modules/nvim/lua/plugins/statuscol.lua
+++ home-manager/modules/nvim/lua/plugins/statuscol.lua
@@ -1,7 +1,7 @@
 return {
   "luukvbaal/statuscol.nvim",
   config = function()
-    -- local builtin = require("statuscol.builtin")
+    local builtin = require("statuscol.builtin")
     require("statuscol").setup({
       -- configuration goes here, for example:
       relculright = true,
@@ -11,10 +11,24 @@ return {
           sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
           click = "v:lua.ScSa",
         },
+        {
+          text = {
+            function(args)
+              return tostring(args.lnum)
+            end,
+          },
+          click = "v:lua.ScLa",
+        },
+        -- {
+        --   text = { " " },
+        -- },
         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
         {
           sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
           click = "v:lua.ScSa",
+        },
+        {
+          text = { " | " },
         },
       },
     })
