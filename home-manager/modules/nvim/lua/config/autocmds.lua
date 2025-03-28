a1439f8a0d415ac8a45d5a835919505262ea5980
James Schurig <james.schurig@rechargeapps.com>
2025-03-28 16:48:55 -0600

    some really good changes.

--- home-manager/modules/nvim/lua/config/autocmds.lua
+++ home-manager/modules/nvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("silent! 1delete _")
+--   end,
+-- })
+
+-- Exclude neotree from getting line number column
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local ft = vim.bo.filetype
+    if ft == "neo-tree" then
+      vim.opt_local.statuscolumn = ""
+    end
+  end,
+})
---
+++ home-manager/modules/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,34 @@
+if true then
+  return {}
+end
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
+  priority = 1000,
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neo-tree Explorer" },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        never_show = {
+          "__pycache__",
+          ".DS_Store",
+          ".ropeproject",
+          ".git",
+        },
+        never_show_by_pattern = {
+          "*.pyc",
+          ".*cache",
+        },
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        always_show_by_pattern = { -- uses glob style patterns
+          -- ".env*",
+        },
+      },
+    },
+  },
+}
