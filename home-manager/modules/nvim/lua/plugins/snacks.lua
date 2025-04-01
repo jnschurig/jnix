f202d884761a1b5607c0bb04329b6872f8a16331
James Schurig <jnschurig@gmail.com>
2025-03-31 23:39:06 -0600

    yes!

--- home-manager/modules/nvim/lua/plugins/snacks.lua
+++ home-manager/modules/nvim/lua/plugins/snacks.lua
@@ -1,16 +1,21 @@
+-- if true then
+--   return {}
+-- end
+
 return {
   {
     "folke/snacks.nvim",
     opts = {
       picker = { enabled = true },
       explorer = { enabled = true },
+      dashboard = { enabled = false },
     },
   },
-  config = function()
-    vim.api.nvim_set_hl(0, "SnacksGitIgnored", { fg = "#a5bcca", bg = "none", italic = true })
-    vim.api.nvim_set_hl(0, "SnacksGitUnstaged", { fg = "#a5bcca", bg = "none", bold = true })
-    vim.api.nvim_set_hl(0, "SnacksPickerGitStatusIgnored", { fg = "#a5bcca", bg = "none", italic = true })
-    vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { fg = "#a5bcca", bg = "none", bold = true })
-    vim.api.nvim_set_hl(0, "SnacksPickerUnselected", { fg = "#a5bcca", bg = "none", bold = true })
-  end,
+  -- config = function()
+  --   vim.api.nvim_set_hl(0, "SnacksGitIgnored", { fg = "#a5bcca", bg = "none", italic = true })
+  --   vim.api.nvim_set_hl(0, "SnacksGitUnstaged", { fg = "#a5bcca", bg = "none", bold = true })
+  --   vim.api.nvim_set_hl(0, "SnacksPickerGitStatusIgnored", { fg = "#a5bcca", bg = "none", italic = true })
+  --   vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { fg = "#a5bcca", bg = "none", bold = true })
+  --   vim.api.nvim_set_hl(0, "SnacksPickerUnselected", { fg = "#a5bcca", bg = "none", bold = true })
+  -- end,
 }
