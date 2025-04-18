d645fd4a0a06b8e10bd3849a7176913bae35aa75
James Schurig <jnschurig@gmail.com>
2025-04-17 23:10:48 -0600

    good

--- home-manager/modules/nvim/lua/plugins/material.lua
+++ home-manager/modules/nvim/lua/plugins/material.lua
@@ -1,8 +1,8 @@
 return {
   "jnschurig/material.nvim",
   -- branch = "color_tweak",
-  lazy = false, -- Ensure it loads on startup
-  priority = 1000, -- Ensure it loads before other plugins
+  lazy = false,
+  priority = 1001,
   config = function()
     -- vim.g.disable_background = true
     vim.g.material_style = "design colors"
@@ -21,11 +21,10 @@ return {
     })
     vim.cmd.colorscheme("material")
     -- vim.api.nvim_set_hl(0, "NonText", { fg = "#a5bcca" })
-    -- config = function()
-    vim.api.nvim_set_hl(0, "SnacksGitIgnored", { fg = "#a5bcca", bg = "none", italic = true })
-    vim.api.nvim_set_hl(0, "SnacksGitUnstaged", { fg = "#a5bcca", bg = "none", bold = true })
-    vim.api.nvim_set_hl(0, "SnacksPickerGitStatusIgnored", { fg = "#a5bcca", bg = "none", italic = true })
-    vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { fg = "#a5bcca", bg = "none", bold = true })
-    vim.api.nvim_set_hl(0, "SnacksPickerUnselected", { fg = "#a5bcca", bg = "none", bold = true })
+    -- vim.api.nvim_set_hl(0, "SnacksGitIgnored", { fg = "#a5bcca", bg = "none", italic = true })
+    -- vim.api.nvim_set_hl(0, "SnacksGitUnstaged", { fg = "#a5bcca", bg = "none", bold = true })
+    -- vim.api.nvim_set_hl(0, "SnacksPickerGitStatusIgnored", { fg = "#a5bcca", bg = "none", italic = true })
+    -- vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { fg = "#a5bcca", bg = "none", bold = true })
+    -- vim.api.nvim_set_hl(0, "SnacksPickerUnselected", { fg = "#a5bcca", bg = "none", bold = true })
   end,
 }
