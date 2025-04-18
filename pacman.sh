b50a2330285fff2bcfbe5ecd163d9d1c0b31a6dd
James Schurig <jnschurig@gmail.com>
2025-04-17 23:23:23 -0600

    adding ruff real quick

--- pacman.sh
+++ pacman.sh
@@ -2,7 +2,7 @@
 
 paru
 sudo pacman -S swayidle ghostty nautilus dolphin popsicle vscode fwupd
-sudo pacman -S neovim lazygit ripgrep fd gsimplecal ripgrep bat
+sudo pacman -S neovim lazygit ripgrep fd gsimplecal ripgrep bat ruff
 sudo pacman -S discord lutris steam dolphin-emu legcord
 # paru -S dolphin-emu-primehack-git # has weird package issues...
 # sudo pacman -Syu --ignore miniupnpc # This is the wierd package issue.
