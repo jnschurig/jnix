94f1d3e9f80f9a9b7b92866d574c009baa72e4d4
James Schurig <jnschurig@gmail.com>
2025-07-22 21:46:59 -0600

    save

--- pacman.sh
+++ pacman.sh
@@ -2,7 +2,7 @@
 
 paru
 sudo pacman -S swayidle ghostty nautilus dolphin popsicle vscode fwupd rofi fuzzel
-sudo pacman -S neovim lazygit ripgrep fd gsimplecal ripgrep bat ruff fzf jq
+sudo pacman -S neovim lazygit ripgrep fd gsimplecal ripgrep bat ruff fzf jq go
 sudo pacman -S discord lutris steam dolphin-emu legcord
 # paru -S dolphin-emu-primehack-git # has weird package issues...
 # sudo pacman -Syu --ignore miniupnpc # This is the wierd package issue.
