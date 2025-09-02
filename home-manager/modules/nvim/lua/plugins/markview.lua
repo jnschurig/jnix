aca4c4e43fdb9a9e3fb5024c42e1361ffb18635d
James Schurig <james.schurig@rechargeapps.com>
2025-09-02 14:05:03 -0600

    Fixing markview and treesitter

--- home-manager/modules/nvim/lua/plugins/markview.lua
+++ home-manager/modules/nvim/lua/plugins/markview.lua
@@ -1,9 +1,21 @@
 return {
   "OXY2DEV/markview.nvim",
-  dependencies = {
-    -- You may not need this if you don't lazy load
-    -- Or if the parsers are in your $RUNTIMEPATH
-    "nvim-treesitter/nvim-treesitter",
-    -- "nvim-tree/nvim-web-devicons",
+  -- dependencies = {
+  -- You may not need this if you don't lazy load
+  -- Or if the parsers are in your $RUNTIMEPATH
+  -- "nvim-treesitter/nvim-treesitter",
+  -- "nvim-tree/nvim-web-devicons",
+  -- },
+
+  -- make sure markview hits the runtimepath before treesitter
+  -- priority = 10000,
+  lazy = true,
+
+  opts = {
+    experimental = {
+      check_rtp = true, -- keep order check on
+      check_rtp_message = true, -- show message if wrong
+      -- set check_rtp_message=false later if you only want to hide the warning
+    },
   },
 }
--- home-manager/modules/nvim/lua/plugins/nvim-treesitter.lua
+++ home-manager/modules/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,13 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "OXY2DEV/markview.nvim" }, -- <-- ensure markview loads first
+    build = ":TSUpdate",
     opts = function(_, opts)
+      opts = opts or {}
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      -- extend the list and include markdown_inline
       vim.list_extend(opts.ensure_installed, {
         "bash",
         "c",
@@ -13,6 +19,7 @@ return {
         "lua",
         "luadoc",
         "markdown",
+        "markdown_inline", -- important for MD highlights
         "nix",
         "norg",
         "python",
@@ -26,7 +33,10 @@ return {
         "vue",
         "yaml",
       })
+
+      opts.indent = opts.indent or {}
       opts.indent.enable = true
+      return opts
     end,
   },
 }
