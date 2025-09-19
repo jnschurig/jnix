40ae6e36a9b9b7cba8ddb7f062acf4079996a3f5
James Schurig <james.schurig@rechargeapps.com>
2025-09-19 16:11:48 -0600

    forgot the file reference

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -3,6 +3,10 @@
     source = ./ghostty/config;
   };
 
+  home.file.".config/ghostty/shaders/cursor_blaze.glsl" = {
+    source = ./ghostty/shaders/cursor_blaze.glsl;
+  };
+
   home.file.".config/ghostty/themes/rose-pine-moon-custom" = {
     source = ./ghostty/themes/rose-pine-moon-custom;
   };
